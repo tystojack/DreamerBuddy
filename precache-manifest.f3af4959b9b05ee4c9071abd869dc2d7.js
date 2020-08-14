2c19197be5a6fa93cc4ff2f1d4cc11c89025de58
tystojack <tystojack@gmail.com>
2020-08-14 15:33:09 -0700

    Updates

---
+++ precache-manifest.f3af4959b9b05ee4c9071abd869dc2d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553771fb65aadb89deae4b9db7820cef",
+    "url": "/DreamBuddy/index.html"
+  },
+  {
+    "revision": "bb3aa77f6531e77c1641",
+    "url": "/DreamBuddy/static/css/main.95862de5.chunk.css"
+  },
+  {
+    "revision": "b1b27b17c213b8dcde9f",
+    "url": "/DreamBuddy/static/js/2.d3a3b7c2.chunk.js"
+  },
+  {
+    "revision": "6da342c569589cff84358090fd1786b7",
+    "url": "/DreamBuddy/static/js/2.d3a3b7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3aa77f6531e77c1641",
+    "url": "/DreamBuddy/static/js/main.258b8b39.chunk.js"
+  },
+  {
+    "revision": "f15ffd0dd80839e3980d",
+    "url": "/DreamBuddy/static/js/runtime-main.fb053bab.js"
+  },
+  {
+    "revision": "334ad1c6248e50cd7f1ab074c75763aa",
+    "url": "/DreamBuddy/static/media/cloud.334ad1c6.png"
+  }
+]);
